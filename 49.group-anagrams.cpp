4851b86e6aaabe660255aa6fb74c192e05043566
zhengjunhao <changchunjunhao@gmail.com>
2020-08-09 16:35:00 +0800

    Better solution on 49

--- 49.group-anagrams.cpp
+++ 49.group-anagrams.cpp
@@ -5,37 +5,22 @@
  */
 
 // @lc code=start
+#include <algorithm>
 #include <map>
 #include <vector>
 #include <string>
-#include <sstream>
 using namespace std;
 class Solution {
 public:
-    string genCode(string& str) {
-        int count[26];
-        memset(count, 0, 26*sizeof(int));
-        for (auto ch : str) {
-            count[ch - 'a']++;
-        }
-        stringstream ss;
-        for (int i = 0; i < 26; i++) {
-            if (count[i] > 0) {
-                ss<<('a'+i) << count[i];
-            }
-        }
-        return ss.str();
+    string genCode(string str) {
+        sort(str.begin(), str.end());
+        return str;
     }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         map<string, vector<string>> code2Strings;
         for (auto& str : strs) {
             const auto& code = genCode(str);
-            auto found = code2Strings.find(code);
-            if (found == code2Strings.end()) {
-                code2Strings.insert(pair(code, vector<string>()));
-                found = code2Strings.find(code);
-            }
-            found->second.push_back(str);
+            code2Strings[code].push_back(str);
         }
         vector<vector<string>> result;
         for (auto& pair : code2Strings) {
